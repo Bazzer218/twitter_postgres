01ff9ab11d97ea94c4c1b351ace92e37bec9eab7
Baron Zeng <bzeng24@cmc.edu>
2024-04-03 10:25:57 -0700

    latest commit

--- load_tweets.sh
+++ load_tweets.sh
@@ -9,9 +9,10 @@ test-data.zip
 
 echo 'load normalized'
 for file in $files; do
+    # call the load_tweets.py file to load data into pg_normalized
 done
 
 echo 'load denormalized'
 for file in $files; do
-    unzip -p 'test-data.zip' | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1333/postgres -c "COPY tweets_jsonb (data) FROM STDIN;"
+    unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:13335 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
